82d3569941700317c2eabae36e86833acf95882c
Sagarved <42365931+Sagarved@users.noreply.github.com>
2024-10-02 09:11:18 -0600

    configuration files

---
+++ config/config.py
@@ -0,0 +1,10 @@
+WEBSITE_URL = 'https://dev.bryteiq.com/login'  # Replace with the actual login page URL
+VALID_USERNAME = ''
+VALID_PASSWORD = ''
+
+#[PROFILE_DETAIL]
+COMPANY_NAME = 'Charter'
+NEW_PASSWORD = 'Charter123$4'
+#[INVALID_PASSWORD]
+INVALID_USERNAME = ''
+INVALID_PASSWORD = 'Charter098$'
